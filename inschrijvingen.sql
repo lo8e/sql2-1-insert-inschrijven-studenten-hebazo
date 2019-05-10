4f74edf65e83e26b8f8ed37104f04b78fd307189
hebazo <48584444+hebazo@users.noreply.github.com>
2019-05-10 14:56:52 +0200

    Update inschrijvingen.sql

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,5 +1,5 @@
 USE lo8e_sql2;
-INSERT INTO klassen (klas_code, slb_code, cohort, opleideing_code)
-VALUE (lo9e-amo1,akr02,2019,amo),
-(lo9e-amo2,bkr02,2019,amo),
-(lo9e-amo3,brc01,2019,amo);
+INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code)
+VALUE ("lo9e-amo1","akr02",2019,"amo"),
+("lo9e-amo2","bkr02",2019,"amo"),
+("lo9e-amo3","brc01",2019,"amo");
