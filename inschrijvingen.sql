da66d02bb2cfa2d6a674483f8727d6925888c9df
hebazo <48584444+hebazo@users.noreply.github.com>
2019-05-10 15:25:46 +0200

    Update inschrijvingen.sql

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,5 +1,4 @@
 USE lo8e_sql2;
-INSERT INTO klassen_studenten (ov_nummer,klas_code)
-VALUES (91234,"lo9e-amo1"),
-(92345,"lo9e-amo2"),
-(93456,"lo9e-amo3");
+UPDATE studenten
+SET geboortedatum = "1998-11-10"
+WHERE ov_nummer = 91234
