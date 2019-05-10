89f7cfa1e4b12e908e93f7d3b2001bcc35049806
hebazo <48584444+hebazo@users.noreply.github.com>
2019-05-10 15:04:01 +0200

    Update inschrijvingen.sql

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -2,4 +2,5 @@ USE lo8e_sql2;
 INSERT INTO studenten (ov_nummer, voornaam,tussenvoegsel,achternaam)
 VALUE (91234,"Ali","","Boo"),
 (92345,"black","van der","Bla"),
-(93456,"Hai","","Bay");
+(93456,"Hai","","Bay"),
+(93456,"boobie","","Boob");
